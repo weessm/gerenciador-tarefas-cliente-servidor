cc90b557de0070eb88c2da3dd94f3e89e072eaba
Thiago Goncalves <thiago.goncalves@cepedi.org.br>
2024-07-05 14:25:10 -0300

    tratamento de lista vazia

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ def handle_client(client_socket, addr):
             if request.startswith('ADD'):
                 _, task = request.split(':', 1)
                 tasks.append(task.strip())
+                print(addr[0])
                 client_socket.send("Tarefa adicionada.".encode('utf-8'))
             elif request.startswith('REMOVE'):
                 _, index = request.split(':', 1)
@@ -32,8 +33,11 @@ def handle_client(client_socket, addr):
                 else:
                     client_socket.send("Índice inválido.".encode('utf-8'))
             elif request.startswith('LIST'):
-                task_list = "\n".join(f"{i}. {task}" for i, task in enumerate(tasks))
-                client_socket.send(task_list.encode('utf-8'))
+                if len(tasks) <= 0:
+                    client_socket.send("Lista vazia".encode('utf-8'))
+                else:
+                    task_list = "\n".join(f"{i}. {task}" for i, task in enumerate(tasks))
+                    client_socket.send(task_list.encode('utf-8'))
             elif request.startswith('EXIT'):
                 print(f"Cliente {addr[0]}:{addr[1]} solicitou desconexão.")
                 break
@@ -52,7 +56,7 @@ def handle_client(client_socket, addr):
 def main():
     global server_running
 
-    host = '192.168.XX.XX'  # Substituir pelo endereço IP do servidor na rede (IPV4)
+    host = '192.168.72.9'  # Substituir pelo endereço IP do servidor na rede (IPV4)
     port = 12345
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
